3942e3b7e244e46643674a8da885e456606c5a63
yadeepak <deepaky19196@gmail.com>
2020-01-16 01:50:21 +0530

    connection to prod credential

--- Client/clientdb.php
+++ Client/clientdb.php
@@ -1,7 +1,7 @@
 <?php
 
-$connection = mysqli_connect("localhost","root","","freelancer");
+//$connection = mysqli_connect("localhost","root","","freelancer");
 
-//$connection = mysqli_connect("localhost","troislog_freeln","troislogic@2019","troislog_freelancer");
+$connection = mysqli_connect("localhost","troislog_freeln","troislogic@2019","troislog_freelancer");
 
 ?>
